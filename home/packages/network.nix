4a94fa4eee9f26b15979ab1e7fdcfdffd97f4f35
Paweł Sobczak <github@fixeq.qzz.io>
2026-06-29 01:40:33 +0200

    refactor(network): convert to nixos module with finit services

--- home/packages/network.nix
+++ home/packages/network.nix
@@ -1,7 +1,32 @@
-{ pkgs, ... }:
+{ pkgs, lib, config, ... }:
+with lib;
+let cfg = config.modules.network; in
 {
-  home.packages = with pkgs; [
-    openssh
-    socat
-  ];
+  options.modules.network.enable = mkEnableOption "NetworkManager and iwd";
+
+  config = mkIf cfg.enable {
+    finit.services.network-manager = {
+      description = "NetworkManager";
+      runlevels = "2345";
+      conditions = [ "service/syslogd/ready" "service/iwd/ready" ];
+      notify = "systemd";
+    };
+
+    finit.services.iwd = {
+      description = "iNet Wireless Daemon";
+      runlevels = "2345";
+      conditions = [ "service/syslogd/ready" ];
+      command = "${pkgs.iwd}/bin/iwd";
+    };
+
+    environment.etc."NetworkManager/conf.d/wifi-backend.conf".text = ''
+      [device]
+      wifi.backend=iwd
+    '';
+
+    environment.systemPackages = with pkgs; [
+      networkmanager
+      networkmanager-openvpn
+    ];
+  };
 }
