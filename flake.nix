c3c32ddb8794da9485ada9bc789f164e41c7cb4d
Paweł <134298056+FixeQD@users.noreply.github.com>
2026-07-13 00:52:23 +0000

    fix: update flake.lock and flake.nix for dependency revisions and temporary branch adjustment

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "finix config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+    # TEMPORARY: On `master` branch
+    nixpkgs.url = "github:NixOS/nixpkgs";
 
     finix.url = "github:FixeQD/finix";
     community-modules.url = "github:finix-community/community-modules";
@@ -28,72 +29,100 @@
     };
   };
 
-  outputs = { nixpkgs, finix, community-modules, disko, sops-nix, zen-browser, spicetify-nix, ... }:
-  let
-    mkHost = { hostname, system ? "x86_64-linux", extraModules ? [ ] }:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-          overlays = [ sops-nix.overlays.default (import ./pkgs) ];
+  outputs =
+    {
+      nixpkgs,
+      finix,
+      community-modules,
+      disko,
+      sops-nix,
+      zen-browser,
+      spicetify-nix,
+      ...
+    }:
+    let
+      mkHost =
+        {
+          hostname,
+          system ? "x86_64-linux",
+          extraModules ? [ ],
+        }:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+            overlays = [
+              sops-nix.overlays.default
+              (import ./pkgs)
+            ];
+          };
+        in
+        finix.lib.finixSystem {
+          inherit (pkgs) lib;
+          modules = [
+            { nixpkgs.pkgs = nixpkgs.lib.mkDefault pkgs; }
+            { _module.args = { inherit zen-browser spicetify-nix; }; }
+            disko.nixosModules.disko
+            community-modules.nixosModules.home-manager
+            community-modules.nixosModules.pipewire
+            ./modules/sops
+            finix.nixosModules.bluetooth
+            finix.nixosModules.docker
+            finix.nixosModules.getty
+            finix.nixosModules.hyprland
+            finix.nixosModules.xwayland-satellite
+            finix.nixosModules.nix-daemon
+            finix.nixosModules.sudo
+            finix.nixosModules.sysklogd
+            finix.nixosModules.iwd
+            finix.nixosModules.dhcpcd
+            finix.nixosModules.sddm
+            finix.nixosModules.zzz
+            ./hosts/${hostname}/default.nix
+          ]
+          ++ extraModules;
         };
-      in
-      finix.lib.finixSystem {
-        inherit (pkgs) lib;
-        modules = [
-          { nixpkgs.pkgs = nixpkgs.lib.mkDefault pkgs; }
-          { _module.args = { inherit zen-browser spicetify-nix; }; }
-          disko.nixosModules.disko
-          community-modules.nixosModules.home-manager
-          community-modules.nixosModules.pipewire
-          ./modules/sops
-          finix.nixosModules.bluetooth
-          finix.nixosModules.docker
-          finix.nixosModules.getty
-          finix.nixosModules.hyprland
-          finix.nixosModules.nix-daemon
-          finix.nixosModules.sudo
-          finix.nixosModules.sysklogd
-          finix.nixosModules.iwd
-          finix.nixosModules.dhcpcd
-          finix.nixosModules.sddm
-          finix.nixosModules.zzz
-          ./hosts/${hostname}/default.nix
-        ] ++ extraModules;
+    in
+    {
+      nixosConfigurations = {
+        hp-zbook = mkHost { hostname = "hp-zbook"; };
       };
-  in
-  {
-    nixosConfigurations = {
-      hp-zbook = mkHost { hostname = "hp-zbook"; };
-    };
 
-    apps.x86_64-linux.install = let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in {
-      type = "app";
-      program = toString (pkgs.writeShellScript "install-hp-zbook" ''
-        export DISKO_BIN="${disko.packages.x86_64-linux.disko}/bin/disko"
-        export SBCTL_BIN="${pkgs.sbctl}/bin/sbctl"
-        export MKPASSWD_BIN="${pkgs.mkpasswd}/bin/mkpasswd"
-        export FLAKE_HOST="hp-zbook"
-        export PRIMARY_USER="fixeq"
-        source ${./install.sh}
-      '');
-    };
+      apps.x86_64-linux.install =
+        let
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        in
+        {
+          type = "app";
+          program = toString (
+            pkgs.writeShellScript "install-hp-zbook" ''
+              export DISKO_BIN="${disko.packages.x86_64-linux.disko}/bin/disko"
+              export SBCTL_BIN="${pkgs.sbctl}/bin/sbctl"
+              export MKPASSWD_BIN="${pkgs.mkpasswd}/bin/mkpasswd"
+              export FLAKE_HOST="hp-zbook"
+              export PRIMARY_USER="fixeq"
+              source ${./install.sh}
+            ''
+          );
+        };
 
-    apps.x86_64-linux.resume-install = let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in {
-      type = "app";
-      program = toString (pkgs.writeShellScript "resume-install-hp-zbook" ''
-        export DISKO_BIN="${disko.packages.x86_64-linux.disko}/bin/disko"
-        export SBCTL_BIN="${pkgs.sbctl}/bin/sbctl"
-        export MKPASSWD_BIN="${pkgs.mkpasswd}/bin/mkpasswd"
-        export FLAKE_HOST="hp-zbook"
-        export PRIMARY_USER="fixeq"
-        export DISKO_MODE="mount"
-        source ${./install.sh}
-      '');
+      apps.x86_64-linux.resume-install =
+        let
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        in
+        {
+          type = "app";
+          program = toString (
+            pkgs.writeShellScript "resume-install-hp-zbook" ''
+              export DISKO_BIN="${disko.packages.x86_64-linux.disko}/bin/disko"
+              export SBCTL_BIN="${pkgs.sbctl}/bin/sbctl"
+              export MKPASSWD_BIN="${pkgs.mkpasswd}/bin/mkpasswd"
+              export FLAKE_HOST="hp-zbook"
+              export PRIMARY_USER="fixeq"
+              export DISKO_MODE="mount"
+              source ${./install.sh}
+            ''
+          );
+        };
     };
-  };
 }
